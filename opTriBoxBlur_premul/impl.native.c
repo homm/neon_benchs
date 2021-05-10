4d60f7da1293535d901de879bacba266b5d337e1
ak <homm86@gmail.com>
2021-05-10 14:31:50 +0300

    float optimisation

--- opTriBoxBlur_premul/impl.native.c
+++ opTriBoxBlur_premul/impl.native.c
@@ -19,9 +19,9 @@ opTriBoxBlur_premul_horz(
     uint32_t d = r * 2 + 1;
     uint32_t r2 = r * 2, r3 = r * 3;
     uint32_t r_mask = all_bits_mask(d);
-    float rev_d = 1.0 / d;
-    float rev_d256 = rev_d / 256.0;
-    float rev_256d = 256.0 * rev_d;
+    uint32_t X1div = (uint32_t) (1 << 24) / d;
+    uint32_t X2div = (uint32_t) (1 << 16) / d;
+    uint32_t X3div = X2div;
     pixel128 X1;
     pixel128 X2;
     pixel128 X3;
@@ -43,10 +43,10 @@ opTriBoxBlur_premul_horz(
             X1.a += sdata[x].a;
         }
 
-        b[0].r = (uint32_t) (X1.r * rev_256d);
-        b[0].g = (uint32_t) (X1.g * rev_256d);
-        b[0].b = (uint32_t) (X1.b * rev_256d);
-        b[0].a = (uint32_t) (X1.a * rev_256d);
+        b[0].r = (uint16_t) ((X1.r * X1div) >> 16);
+        b[0].g = (uint16_t) ((X1.g * X1div) >> 16);
+        b[0].b = (uint16_t) ((X1.b * X1div) >> 16);
+        b[0].a = (uint16_t) ((X1.a * X1div) >> 16);
         X2.r = b[0].r * (r + 1);
         X2.g = b[0].g * (r + 1);
         X2.b = b[0].b * (r + 1);
@@ -56,20 +56,20 @@ opTriBoxBlur_premul_horz(
             X1.g += sdata[x + r].g - sdata[0].g;
             X1.b += sdata[x + r].b - sdata[0].b;
             X1.a += sdata[x + r].a - sdata[0].a;
-            b[x].r = (uint32_t) (X1.r * rev_256d);
-            b[x].g = (uint32_t) (X1.g * rev_256d);
-            b[x].b = (uint32_t) (X1.b * rev_256d);
-            b[x].a = (uint32_t) (X1.a * rev_256d);
+            b[x].r = (uint16_t) ((X1.r * X1div) >> 16);
+            b[x].g = (uint16_t) ((X1.g * X1div) >> 16);
+            b[x].b = (uint16_t) ((X1.b * X1div) >> 16);
+            b[x].a = (uint16_t) ((X1.a * X1div) >> 16);
             X2.r += b[x].r;
             X2.g += b[x].g;
             X2.b += b[x].b;
             X2.a += b[x].a;
         }
 
-        c[0].r = (uint32_t) (X2.r * rev_d);
-        c[0].g = (uint32_t) (X2.g * rev_d);
-        c[0].b = (uint32_t) (X2.b * rev_d);
-        c[0].a = (uint32_t) (X2.a * rev_d);
+        c[0].r = (uint16_t) ((X2.r * X2div) >> 16);
+        c[0].g = (uint16_t) ((X2.g * X2div) >> 16);
+        c[0].b = (uint16_t) ((X2.b * X2div) >> 16);
+        c[0].a = (uint16_t) ((X2.a * X2div) >> 16);
         X3.r = c[0].r * (r + 2);
         X3.g = c[0].g * (r + 2);
         X3.b = c[0].b * (r + 2);
@@ -79,18 +79,18 @@ opTriBoxBlur_premul_horz(
             X1.g += sdata[x + r2].g - sdata[x - 1].g;
             X1.b += sdata[x + r2].b - sdata[x - 1].b;
             X1.a += sdata[x + r2].a - sdata[x - 1].a;
-            b[x + r].r = (uint32_t) (X1.r * rev_256d);
-            b[x + r].g = (uint32_t) (X1.g * rev_256d);
-            b[x + r].b = (uint32_t) (X1.b * rev_256d);
-            b[x + r].a = (uint32_t) (X1.a * rev_256d);
+            b[x + r].r = (uint16_t) ((X1.r * X1div) >> 16);
+            b[x + r].g = (uint16_t) ((X1.g * X1div) >> 16);
+            b[x + r].b = (uint16_t) ((X1.b * X1div) >> 16);
+            b[x + r].a = (uint16_t) ((X1.a * X1div) >> 16);
             X2.r += b[x + r].r - b[0].r;
             X2.g += b[x + r].g - b[0].g;
             X2.b += b[x + r].b - b[0].b;
             X2.a += b[x + r].a - b[0].a;
-            c[x].r = (uint32_t) (X2.r * rev_d);
-            c[x].g = (uint32_t) (X2.g * rev_d);
-            c[x].b = (uint32_t) (X2.b * rev_d);
-            c[x].a = (uint32_t) (X2.a * rev_d);
+            c[x].r = (uint16_t) ((X2.r * X2div) >> 16);
+            c[x].g = (uint16_t) ((X2.g * X2div) >> 16);
+            c[x].b = (uint16_t) ((X2.b * X2div) >> 16);
+            c[x].a = (uint16_t) ((X2.a * X2div) >> 16);
             X3.r += c[x].r;
             X3.g += c[x].g;
             X3.b += c[x].b;
@@ -107,29 +107,30 @@ opTriBoxBlur_premul_horz(
             X1.g += sdata[x + r3].g - sdata[x + r - 1].g;
             X1.b += sdata[x + r3].b - sdata[x + r - 1].b;
             X1.a += sdata[x + r3].a - sdata[x + r - 1].a;
-            last_b.r = b[(x + r2) & r_mask].r = (uint32_t) (X1.r * rev_256d);
-            last_b.g = b[(x + r2) & r_mask].g = (uint32_t) (X1.g * rev_256d);
-            last_b.b = b[(x + r2) & r_mask].b = (uint32_t) (X1.b * rev_256d);
-            last_b.a = b[(x + r2) & r_mask].a = (uint32_t) (X1.a * rev_256d);
+            last_b.r = b[(x + r2) & r_mask].r = (uint16_t) ((X1.r * X1div) >> 16);
+            last_b.g = b[(x + r2) & r_mask].g = (uint16_t) ((X1.g * X1div) >> 16);
+            last_b.b = b[(x + r2) & r_mask].b = (uint16_t) ((X1.b * X1div) >> 16);
+            last_b.a = b[(x + r2) & r_mask].a = (uint16_t) ((X1.a * X1div) >> 16);
             X2.r += last_b.r - b[(x - 1) & r_mask].r;
             X2.g += last_b.g - b[(x - 1) & r_mask].g;
             X2.b += last_b.b - b[(x - 1) & r_mask].b;
             X2.a += last_b.a - b[(x - 1) & r_mask].a;
-            last_c.r = c[(x + r) & r_mask].r = (uint32_t) (X2.r * rev_d);
-            last_c.g = c[(x + r) & r_mask].g = (uint32_t) (X2.g * rev_d);
-            last_c.b = c[(x + r) & r_mask].b = (uint32_t) (X2.b * rev_d);
-            last_c.a = c[(x + r) & r_mask].a = (uint32_t) (X2.a * rev_d);
+            last_c.r = c[(x + r) & r_mask].r = (uint16_t) ((X2.r * X2div) >> 16);
+            last_c.g = c[(x + r) & r_mask].g = (uint16_t) ((X2.g * X2div) >> 16);
+            last_c.b = c[(x + r) & r_mask].b = (uint16_t) ((X2.b * X2div) >> 16);
+            last_c.a = c[(x + r) & r_mask].a = (uint16_t) ((X2.a * X2div) >> 16);
             X3.r += last_c.r - c[(x - r - 1) & r_mask].r;
             X3.g += last_c.g - c[(x - r - 1) & r_mask].g;
             X3.b += last_c.b - c[(x - r - 1) & r_mask].b;
             X3.a += last_c.a - c[(x - r - 1) & r_mask].a;
 
-            rdata[x * Rimg->xsize] = (pixel32){
-                (uint8_t) (X3.r * rev_d256),
-                (uint8_t) (X3.g * rev_d256),
-                (uint8_t) (X3.b * rev_d256),
-                (uint8_t) (X3.a * rev_d256)
+            *rdata = (pixel32){
+                (uint8_t) ((X3.r * X3div) >> 24),
+                (uint8_t) ((X3.g * X3div) >> 24),
+                (uint8_t) ((X3.b * X3div) >> 24),
+                (uint8_t) ((X3.a * X3div) >> 24)
             };
+            rdata += Rimg->xsize;
         }
     }
 }
